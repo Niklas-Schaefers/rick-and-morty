c4599b910e24efd808bf1376fdd46f2083c47a72
Niklas-Schaefers <Niklas.Schaefers@gmx.de>
2021-05-04 16:05:24 +0200

    ✨ Add styling and Create Characters

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
+import { createCharacterElements } from "./components/characters.js";
 import "./style.css";
-import { createElement } from "./utils/elements";
+import { createElement } from "./utils/elements.js";
 
-const mainElement = createElement("main", {
-  className: "main",
+const header = createElement("header", {
+  className: "hero",
   children: [
-    createElement("input", { placeholder: "Enter name" }),
-    createElement("section", { innerText: "Results" }),
+    createElement("h1", {
+      className: "header__title",
+      innerText: "Ricky and Morty",
+    }),
+    createElement("input", {
+      className: "input",
+      placeholder: "Search character",
+      autofocus: true,
+    }),
   ],
 });
 
-document.querySelector("#app").append(mainElement);
+const section = createElement("section", {
+  className: "results",
+  children: [
+    createCharacterElements({
+      imgSrc: "https://rickandmortyapi.com/api/character/avatar/361.jpeg",
+      name: "Rick",
+      status: "Alive",
+      species: "Human",
+      location: {
+        name: "Earth",
+        url: "https://rickandmortyapi.com/api/location/20",
+      },
+    }),
+  ],
+});
+
+const footer = createElement("footer", {
+  className: "footer",
+  children: [
+    createElement("div", {
+      className: "footer__elements",
+      children: [
+        createElement("span", { innerText: "This is" }),
+        createElement("span", { innerText: " the footer" }),
+      ],
+    }),
+  ],
+});
+
+document.querySelector("#app").append(header, section, footer);
