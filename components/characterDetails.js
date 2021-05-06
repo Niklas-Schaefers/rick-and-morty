c4693a51f805d0b7518f7fbd2ca09973cc5f7787
Niklas Schäfers <Niklas.Schaefers@gmx.de>
2021-05-06 17:36:01 +0200

    ✨ Finalize

--- components/characterDetails.js
+++ components/characterDetails.js
@@ -1,21 +1,16 @@
-import { createElement } from "../utils/elements";
-import "./characters.css";
+import { createElement } from "../utils/elements.js";
 
-export function createCharacterElements({
+export function createCharacterDetails({
   image,
   name,
   status,
   species,
   location,
-  id,
 }) {
   return createElement("div", {
     className: "character-card",
     children: [
-      createElement("a", {
-        href: `details.html?id=${id}`,
-        children: [createElement("img", { src: image })],
-      }),
+      createElement("img", { src: image }),
       createElement("h2", { innerText: name }),
       createElement("p", { innerText: "Status: " + status }),
       createElement("p", { innerText: "Species: " + species }),
--- utils/api.js
+++ utils/api.js
@@ -1,20 +1,3 @@
-// export function getCharacters(name) {
-//   return fetch(`https://rickandmortyapi.com/api/character?name=${name}`)
-//     .then((response) => response.json())
-//     .then((response) => response.results);
-// }
-
-// export async function getCharacters(name) {
-//   const promise = fetch(`https://rickandmortyapi.com/api/character?name=${name}`)
-//   const response = await promise;
-// if (response.status === 404) {
-//   return [];
-// }
-//   const data = await response.json();
-//   return data.results;
-
-//
-
 export async function getCharacters(name) {
   const promise = fetch(
     `https://rickandmortyapi.com/api/character?name=${name}`
@@ -29,4 +12,14 @@ export async function getCharacters(name) {
 
 export function removeChildren(event) {
   event.innerHTML = "";
+}
+
+export async function getCharacter(id) {
+  const promise = fetch(`https://rickandmortyapi.com/api/character/${id}`);
+  const response = await promise;
+  if (response.status === 404) {
+    return {};
+  }
+  const data = await response.json();
+  return data;
 }
